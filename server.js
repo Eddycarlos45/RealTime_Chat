6ae1e79e949a46132013f60e3621d636a52fe968
Edson Sousa <edsoncarlos33@hotmail.com>
2021-01-12 11:33:11 -0300

    Initial configuration socket.io

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+//Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...')
+
+    socket.emit('message','Welcome to ChatCord!')
+})
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
